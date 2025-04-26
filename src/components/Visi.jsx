cd43e6f1850368e7184e8f9c64ec10fe0b342da7
Hafiz17 <119962175+RIKI1796@users.noreply.github.com>
2025-04-26 14:48:01 +0700

    Add files via upload

---
+++ src/components/Visi.jsx
@@ -0,0 +1,49 @@
+import "./styles/Visi.css";
+import Profile from "../assets/pakikbal.jpg";
+import Ketua from "../assets/china.jpg"
+
+function Visi() {
+  return (
+    <>
+      <section className="visi-misi-section">
+        <h1 className="section-title">Visi & Misi</h1>
+        <div className="container-visi-misi">
+          <div className="left-side">
+            <h2 className="wali-kelas-text">Wali Kelas</h2>
+            <div className="member-picture-box">
+              <img
+                className="member-picture"
+                src={Profile}
+                alt="Wali Kelas"
+              />
+            </div>
+          </div>
+
+          <div className="right-side">
+            <div className="visi-misi-text">
+              <h2 className="visi">Visi</h2>
+              <p className="visi-text">"Makan Gak Makan Yang Penting Kumpul"</p>
+
+              <h2 className="misi">Misi</h2>
+              <p className="misi-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas quidem commodi laudantium excepturi. Sint a labore
+                tempora?
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="quote-box">
+          <img className="visimisi-logo" src={Ketua} alt="Pak Ikbal" />
+          <h2 className="text-visimisi">
+            "Tetap patah semangat karena jodoh hanya di tangan tuhan"
+            <br />
+            -Ketua Kelas-
+          </h2>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Visi;
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,19 @@
+import Navbar from "../components/Navbar.jsx";
+import Hero from "../components/Hero.jsx";
+import Visi from "../components/Visi.jsx";
+import Card from "../components/Card.jsx";
+import Bottomsec from "../components/Bottomsec.jsx";
+
+function Pages() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Visi />
+      <Card />
+      <Bottomsec />
+    </>
+  );
+}
+
+export default Pages;
